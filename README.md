1b98a562dfef0dfbc23ea0e78970d0d383e4260a
Codeyg12 <codey.gallup@gmail.com>
2023-07-02 14:50:14 -0600

    updating schema and table in Graph.tsx, updating rows from those changes into DataManipulators.ts, and some thoughts added to the README

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # JPMC Task 3
 Starter repo for task 3 of JPMC's Forage program
+
+## Explanation
+After following the steps I attempted to change the upper and lower bounds but it wasn't reaching near the upper bound no matter how long the time spent. I ended up going with the original number that was proposed minus the operator due to it being a fixed number. If we were to add the option to change the bounds I would change it back to using 1 + `upper_bound_variable` or 1 - `lower_bound_variable`. If the desire was to keep it a fixed range, i.e. 0.05, I would add a `bound_variable`.
--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,40 @@
-import { ServerRespond } from './DataStreamer';
+import { ServerRespond } from "./DataStreamer";
 
+// modify to match the changes on the schema in Graph.tsx
 export interface Row {
-  stock: string,
-  top_ask_price: number,
-  timestamp: Date,
+  price_abc: number;
+  price_def: number;
+  ratio: number;
+  timestamp: Date;
+  upper_bound: number;
+  lower_bound: number;
+  trigger_alert: number | undefined;
 }
 
-
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+  static generateRow(serverRespond: ServerRespond[]): Row {
+    // both prices will follow the same formula of top_ask and top_bid divided in half
+    const priceABC =
+      (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) / 2;
+    const priceDEF =
+      (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) / 2;
+    const ratio = priceABC / priceDEF;
+    const upperBound = 1.05;
+    const lowerBound = .95;
+    return {
+      price_abc: priceABC,
+      price_def: priceDEF,
+      ratio,
+      // ternary to return which timestamp is higher
+      timestamp:
+        serverRespond[0].timestamp > serverRespond[1].timestamp
+          ? serverRespond[0].timestamp
+          : serverRespond[1].timestamp,
+      upper_bound: upperBound,
+      lower_bound: lowerBound,
+      // ternary to return the ratio if its past the bounds otherwise remain undefined
+      trigger_alert:
+        ratio > upperBound || ratio < lowerBound ? ratio : undefined,
+    };
   }
 }
